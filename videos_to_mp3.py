45f40b6967d6f8b9a83b08454f58331582abd586
Yashwardhan Rathore <yashwardhan2027@gmail.com>
2026-05-27 17:33:35 +0530

    Revisited the project and added coments in the code

--- videos_to_mp3.py
+++ videos_to_mp3.py
@@ -8,3 +8,8 @@ for file in files:
     file_name = file.split(" ｜ ")[0]
     print( tutorial_number,  file_name)
     subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
+
+    #ffmpeg is a powerful multimedia framework. In this code, we are using ffmpeg to convert video files to mp3 audio files.
+    
+    #subprocess is used to run the ffmpeg command in the terminal, which converts the video file to mp3 format and saves it in the audios folder with a new name that includes the tutorial number and file name.
+
